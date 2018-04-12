4ca78e48c9d913edbd647b41d2ecade83d6cf961
Evan Atherton <evan.atherton@autodesk.com>
2018-04-11 22:29:35 -0700

    small UI spacing update

--- mimic/scripts/mimic_ui.py
+++ mimic/scripts/mimic_ui.py
@@ -1171,7 +1171,7 @@ def _build_hotkeys_frame(parent_layout):
 
 def _build_ui_prefs_frame(parent_layout):
     pm.frameLayout(label="UI", collapsable=True)
-    pm.columnLayout(adj=True, columnAttach=('both', 5))
+    ui_prefs_column = pm.columnLayout(adj=True, columnAttach=('both', 5))
 
     pm.separator(height=2, style='none')
 
@@ -1196,12 +1196,15 @@ def _build_ui_prefs_frame(parent_layout):
               width=50,
               height=20,
               command=mimic_utils.set_shader_range)
+    
+    pm.setParent(ui_prefs_column)
+    pm.separator(height=8, style='none')
     pm.setParent(parent_layout)
 
 
 def _build_performance_prefs_frame(parent_layout):
     pm.frameLayout(label="Performance", collapsable=True)
-    pm.columnLayout(adj=True, columnAttach=('both', 5))
+    performance_prefs_column = pm.columnLayout(adj=True, columnAttach=('both', 5))
 
     pm.separator(height=2, style='none')
 
@@ -1211,7 +1214,9 @@ def _build_performance_prefs_frame(parent_layout):
                 annotation='If checked, reconcileRotation script job' \
                            'will run',
                 value=mimic_config.EXECUTE_RECONCILE_ROTATION_DEFAULT)
-
+    
+    pm.setParent(performance_prefs_column)
+    pm.separator(height=8, style='none')
     pm.setParent(parent_layout)
 
 
